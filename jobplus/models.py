d1e299d43c06ef82aa938babd6759ce346e904cf
SKY-20150516HYE\Administrator <zmxhdu@126.com>
2017-11-27 00:33:29 +0800

    添加user，status #%

--- jobplus/models.py
+++ jobplus/models.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -15,4 +16,72 @@ class Base(db.Model):
     updated_time = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
-class 
+class User(Base, UserMixin):
+    __tablename__ = 'user'
+
+    ROLE_USER = 10
+    ROLE_COMPANY = 20
+    ROLE_ADMIN = 30
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, index=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    _password = db.Column('password', db.String(256), nullable=False)
+
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+
+    def __repr__(self):
+        return '<User:{}>'.format(self.username)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, orig_password):
+        self._password = generate_password_hash(orig_password)
+
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == self.ROLE_ADMIN
+
+    @property
+    def is_company(self):
+        return self.role == self.ROLE_COMPANY
+
+
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return '<Job:{}>'.format(self.name)
+
+
+class Company(Base):
+    __tablename__ = 'company'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return '<Company:{}>'.format(self.name)
+
+
+class Status(Base):
+    __tablename__ = 'status'
+
+    # 等待企业审核
+    STATUS_WAITING = 1
+    # 被拒绝
+    STATUS_REJECT = 2
+    # 被接收，等待通知面试
+    STATUS_ACCEPT = 3
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL'))
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id', ondelete='SET NULL'))
+    status = db.Column(db.SmallInteger, default=STATUS_WAITING)
