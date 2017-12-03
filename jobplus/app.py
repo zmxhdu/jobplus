f326c110404177994893dd3370eee2bd3c9c92a1
SKY-20150516HYE\Administrator <zmxhdu@126.com>
2017-12-03 17:32:27 +0800

    models完善 fix #5

--- jobplus/app.py
+++ jobplus/app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from jobplus.config import configs
+from jobplus.models import db
 
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
+    db.init_app(app)
+    Migrate(app, db)
 
     return app
--- jobplus/models.py
+++ jobplus/models.py
@@ -1,6 +1,5 @@
 # coding = utf-8
 from datetime import datetime
-from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -16,6 +15,13 @@ class Base(db.Model):
     updated_time = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
+user_job = db.Table(
+    'user_job',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE')),
+    db.Column('job_id', db.Integer, db.ForeignKey('job.id', ondelete='CASCADE'))
+)
+
+
 class User(Base, UserMixin):
     __tablename__ = 'user'
 
@@ -27,8 +33,11 @@ class User(Base, UserMixin):
     username = db.Column(db.String(32), unique=True, index=True, nullable=False)
     email = db.Column(db.String(64), unique=True, index=True, nullable=False)
     _password = db.Column('password', db.String(256), nullable=False)
-
     role = db.Column(db.SmallInteger, default=ROLE_USER)
+    collect_jobs = db.relationship('Job', secondary=user_job)
+    job_experiences = db.Column(db.String(1024))
+    edu_experiences = db.Column(db.String(1024))
+    project_experiences = db.Column(db.String(1024))
 
     def __repr__(self):
         return '<User:{}>'.format(self.username)
@@ -57,6 +66,14 @@ class Job(Base):
     __tablename__ = 'job'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24))
+    salary_low = db.Column(db.Integer, nullable=False)
+    salary_high = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(24))
+    tags = db.Column(db.String(128))
+    job_requirement = db.Column(db.String(1024))
+    copmany_id = db.Column(db.Integer, db.ForeignKey('company.id', ondelete='CASCADE'))
+    copmany = db.relationship('Company', uselist=False)
 
     def __repr__(self):
         return '<Job:{}>'.format(self.name)
@@ -66,6 +83,13 @@ class Company(Base):
     __tablename__ = 'company'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False, index=True, unique=True)
+    email = db.Column(db.String(24), nullable=False)
+    location = db.Column(db.String(24), nullable=False)
+    # 公司描述
+    description = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL'))
+    user = db.relationship('User', uselist=False, backref=db.backref('company', uselist=False))
 
     def __repr__(self):
         return '<Company:{}>'.format(self.name)
